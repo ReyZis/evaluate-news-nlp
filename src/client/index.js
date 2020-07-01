b56df38583cfb38e097fde194ed061cda9d2f9d1
mohamed rami <ramidz130@gmail.com>
2020-07-01 13:44:38 +0100

    add the./ in the import so the styles appear now

--- src/client/index.js
+++ src/client/index.js
@@ -6,11 +6,11 @@ import { handleSubmit } from './js/formHandler'
 
 alert("I EXIST")
 
-import 'styles/base.scss'
-import 'styles/footer.scss'
-import 'styles/form.scss'
-import 'styles/header.scss'
-import 'styles/resets.scss'
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
 
 
 console.log(checkForName);
--- webpack.dev.js
+++ webpack.dev.js
@@ -7,14 +7,23 @@ module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
-    stats: 'verbose',
     module: {
-        rules: [
-            {
+        rules: [{
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
         ]
     },
     plugins: [
