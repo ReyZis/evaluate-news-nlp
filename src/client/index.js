f403d2a72bff7cddfbfe8f654c89619d8c02e375
mohamed rami <ramidz130@gmail.com>
2020-07-01 12:49:28 +0100

    after audit fix and  after hanging the port to 3000

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,12 @@
+alert("I EXIST")
+
+
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
+alert("I EXIST")
+
 console.log(checkForName);
 
-alert("I EXIST")
 console.log("CHANGE!!");
+alert("I EXIST")
