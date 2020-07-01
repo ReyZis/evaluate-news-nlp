f58a946aadb357d4eb0ba4167a38bc41d154d8d9
mohamed rami <ramidz130@gmail.com>
2020-07-01 13:48:44 +0100

    fixing the events

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,5 @@
-alert("I EXIST")
-
-
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
-
-alert("I EXIST")
 
 import './styles/resets.scss'
 import './styles/base.scss'
@@ -12,8 +7,9 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
-
-console.log(checkForName);
-
-console.log("CHANGE!!");
 alert("I EXIST")
+
+export {
+    checkForName,
+    handleSubmit
+}
