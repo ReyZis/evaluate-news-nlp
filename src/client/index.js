d246d5b29f24c6fb750cb3cfe88d0eb84d9ee771
mohamed rami <ramidz130@gmail.com>
2020-07-01 12:55:34 +0100

    changing the styles to scss

--- src/client/index.js
+++ src/client/index.js
@@ -6,6 +6,13 @@ import { handleSubmit } from './js/formHandler'
 
 alert("I EXIST")
 
+import 'styles/base.scss'
+import 'styles/footer.scss'
+import 'styles/form.scss'
+import 'styles/header.scss'
+import 'styles/resets.scss'
+
+
 console.log(checkForName);
 
 console.log("CHANGE!!");
